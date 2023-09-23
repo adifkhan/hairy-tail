20982f84eee98df2a8c66f5d8a8dc2ff11ff0ea7
adifkhan <pkadif@gmail..com>
2023-09-23 18:03:16 +0600

    routes initialized

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -4,7 +4,7 @@ const FrequentlyAskedQue = () => {
   return (
     <div>
       <div style={{ textAlign: "center", padding: "5rem" }}>
-        <h1>Don&apos;t stop exploring...</h1>
+        <h1 style={{ marginBottom: "3rem" }}>Never Stop Exploring...</h1>
         <h2>FAQ page is under construction!</h2>
       </div>
     </div>
--- src/app/salons/page.js
+++ src/app/salons/page.js
@@ -3,7 +3,7 @@ import React from "react";
 const Salons = () => {
   return (
     <div style={{ textAlign: "center", padding: "5rem" }}>
-      <h1>Don&apos;t stop exploring...</h1>
+      <h1 style={{ marginBottom: "3rem" }}>Never Stop Exploring...</h1>
       <h2>Salons page is under construction!</h2>
     </div>
   );
